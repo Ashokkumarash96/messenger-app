4f747ce5157a8264b31172f95a718f0141369017
Ashokkumar <ashokkumarash96@gmail.com>
2023-02-09 20:16:42 +0530

    completed

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,25 @@ const sendButton = document.getElementById("send-button");
 let msg = document.querySelector("disp");
 let ps1 = document.getElementById("ps1");
 let ps2 = document.getElementById("ps2");
+const time = new Date();
 
 sendButton.addEventListener("click", function () {
   if (person1.value !== "") {
     //alert(`Person 1 says: ${person1.value}`);
     let p = document.createElement("p");
-    p.innerHTML = `${person1.value}`;
+    const time = new Date();
+    p.innerHTML = `${
+      person1.value
+    } <t>${time.getHours()} : ${time.getMinutes()}</t>`;
     ps1.appendChild(p);
     person1.value = "";
   } else if (person2.value !== "") {
     //alert(`Person 2 says: ${person2.value}`);
     let span = document.createElement("span");
-    span.innerHTML = `${person2.value}`;
+    const time = new Date();
+    span.innerHTML = `${
+      person2.value
+    } <t>${time.getHours()} : ${time.getMinutes()}</t>`;
     ps1.appendChild(span);
     person2.value = "";
   }
